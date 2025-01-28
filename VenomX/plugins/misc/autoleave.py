72e0848ee656cd9920ea43fc72582741de23282a
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2025-01-29 02:30:05 +0530

    Update autoleave.py

--- VenomX/plugins/misc/autoleave.py
+++ VenomX/plugins/misc/autoleave.py
@@ -12,7 +12,7 @@ from VenomX.utils.database import get_client, is_active_chat, is_autoend
 async def auto_leave():
     if config.AUTO_LEAVING_ASSISTANT = str(True):
         while not await asyncio.sleep(
-            config.AUTO_LEAVE_ASSISTANT_TIME
+            config.AUTO_LEAVING_ASSISTANT:
         ):
             from VenomX.core.userbot import assistants
 
