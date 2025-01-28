005bcfe35f0c5e037505a9f7e13d57c855b75d45
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2025-01-28 22:43:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALL_QUIZ_TAME")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BWF_MUSIC1")
 
 
-AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "false")
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 AUTO_SUGGESTION_TIME = int(
     getenv("AUTO_SUGGESTION_TIME", "5400"))
 # Get this credentials from https://developer.spotify.com/dashboard
