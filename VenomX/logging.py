ba27e58268b41186c3d14e4a72345fd2395dbaa2
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2025-01-31 21:39:38 +0530

    Update logging.py

--- VenomX/logging.py
+++ VenomX/logging.py
@@ -1,20 +1,24 @@
 import logging
 
+# Configure logging
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        logging.FileHandler("log.txt"),
-        logging.StreamHandler(),
+        logging.FileHandler("log.txt"),  # Log to a file
+        logging.StreamHandler(),         # Log to console
     ],
 )
 
+# Set logging levels for specific modules
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pymongo").setLevel(logging.ERROR) 
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 
-
 def LOGGER(name: str) -> logging.Logger:
+    """
+    Returns a logger instance with the given name.
+    """
     return logging.getLogger(name)
