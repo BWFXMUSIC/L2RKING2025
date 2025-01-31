80e74fc3961abefe080c8cd681bd2f37cdb2deed
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2025-01-31 21:44:47 +0530

    Update __init__.py

--- VenomX/__init__.py
+++ VenomX/__init__.py
@@ -3,24 +3,25 @@ from VenomX.core.dir import dirr
 from VenomX.core.git import git
 from VenomX.core.userbot import Userbot
 from VenomX.misc import dbb, heroku
-
 from .logging import LOGGER
 
+# Function calls for initialization
 dirr()
 git()
 dbb()
 heroku()
 
+# Create instances
 app = Ayush()
 userbot = Userbot()
 
+# Platform APIs
+from .platforms import AppleAPI, CarbonAPI, SoundAPI, SpotifyAPI, RessoAPI, TeleAPI, YouTubeAPI
 
-from .platforms import *
-
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
-Resso = RessoAPI()
-Telegram = TeleAPI()
-YouTube = YouTubeAPI()
+apple = AppleAPI()
+carbon = CarbonAPI()
+soundcloud = SoundAPI()
+spotify = SpotifyAPI()
+resso = RessoAPI()
+telegram = TeleAPI()
+youtube = YouTubeAPI()
